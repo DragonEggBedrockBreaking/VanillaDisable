dc78348272c1396c5a8730c26bcd3c0044091b9d
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-24 11:16:05 +0000

    Switch from Gradle to Brachyura

--- README.md
+++ README.md
@@ -8,6 +8,21 @@ You can use gamerules to enable/disable a lot more stuff with this mod.
 
 This mod is incomplete/experimental. Features may be added and removed at any time.
 
+## Compiling
+
+Unlike most minecraft mods, this mod does not use the maven or gradle build systems.
+This mod uses a new, faster, more minecraft-focused build system called [Brachyura](https://github.com/CoolCrabs/brachyura).
+
+To compile the project, run the commands (prequisite: you need Java version 17 or above installed and in path):
+```
+git clone https://github.com/DragonEggBedrockBreaking/VanillaDisable.git/
+cd VanillaDisable
+java -jar brachyura-bootstrap-0.jar build
+```
+
+You can speed it up by using the `-j n`, where n is the number of threads that you want to use.
+The artifacts will be in `build/libs/`, just like with the other builds systems.
+
 ## License
 
 This mod is available under the LGPL-3.0 license.
---
+++ buildscript/src/main/java/Buildscript.java
@@ -0,0 +1,79 @@
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import io.github.coolcrabs.brachyura.decompiler.BrachyuraDecompiler;
+import io.github.coolcrabs.brachyura.dependency.JavaJarDependency;
+import io.github.coolcrabs.brachyura.fabric.FabricLoader;
+import io.github.coolcrabs.brachyura.fabric.FabricMaven;
+import io.github.coolcrabs.brachyura.fabric.FabricProject;
+import io.github.coolcrabs.brachyura.fabric.Yarn;
+import io.github.coolcrabs.brachyura.maven.MavenId;
+import io.github.coolcrabs.brachyura.util.AtomicFile;
+import io.github.coolcrabs.brachyura.util.Util;
+import net.fabricmc.mappingio.tree.MappingTree;
+
+public class Buildscript extends FabricProject {
+    @Override
+    public String getMcVersion() {
+        // Minecraft Version
+        return "1.18.1";
+    }
+
+    @Override
+    public MappingTree createMappings() {
+        // Yarn Mappings Version
+        return Yarn.ofMaven(FabricMaven.URL, FabricMaven.yarn("1.18.1+build.7")).tree;
+    }
+
+    @Override
+    public FabricLoader getLoader() {
+        // Fabric Loader Version
+        return new FabricLoader(FabricMaven.URL, FabricMaven.loader("0.12.12"));
+    }
+
+    @Override
+    public String getModId() {
+        // Mod Name
+        return "vanilla_disable";
+    }
+
+    @Override
+    public String getVersion() {
+        // Mod Version
+        return "0.2.0";
+    }
+
+    @Override
+    public void getModDependencies(ModDependencyCollector d) {
+        // Fabric API
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-resource-loader-v0", "0.4.11+3ac43d9514"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-game-rule-api-v1", "1.0.10+3ac43d9514"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+        // DataBreaker
+        d.addMaven("https://maven.gegy.dev/", new MavenId("supercoder79:databreaker:0.2.8"), ModDependencyFlag.RUNTIME);
+        // Tiefix (modrinth special lol)
+        //d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth:tiefix:1.7.0"), ModDependencyFlag.RUNTIME);
+        try {
+            Path target = getLocalBrachyuraPath().resolve("tiefix-1.7.0.jar");
+            if (!Files.exists(target)) {
+                try (
+                    AtomicFile f = new AtomicFile(target);
+                    InputStream is = new URL("https://api.modrinth.com/maven/maven/modrinth/tiefix/1.7.0/tiefix-1.7.0.jar").openStream();
+                ) {
+                    Files.copy(is, f.tempPath, StandardCopyOption.REPLACE_EXISTING);
+                    f.commit();
+                    System.out.println("INFO: Remapped tiefix-1.7.0-sources.jar");
+                }
+            }
+            d.add(new JavaJarDependency(target, null, null), ModDependencyFlag.RUNTIME);
+        } catch (Exception e) {
+            Util.sneak(e);
+        }
+    }
+
+    @Override
+    public BrachyuraDecompiler decompiler() {
+        return null;
+    }
+}
