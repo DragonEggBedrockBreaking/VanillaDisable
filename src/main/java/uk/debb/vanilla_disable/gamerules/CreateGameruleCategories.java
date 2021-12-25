366ee132a83b0b1b019a385f329b6eb1b3aa1321
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-25 01:27:55 +0000

    Specify VanillaDisabled in gamerule categories

--- src/main/java/uk/debb/vanilla_disable/gamerules/CreateGameruleCategories.java
+++ src/main/java/uk/debb/vanilla_disable/gamerules/CreateGameruleCategories.java
@@ -6,15 +6,15 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
 public class CreateGameruleCategories {
-    public static final CustomGameRuleCategory DAMAGE = new CustomGameRuleCategory(
-        new Identifier("gamerule.category.damage"),
-        new TranslatableText("gamerule.category.damage")
+    public static final CustomGameRuleCategory VD_DAMAGE = new CustomGameRuleCategory(
+        new Identifier("gamerule.category.vd.damage"),
+        new TranslatableText("gamerule.category.vd.damage")
             .formatted(Formatting.BOLD)
             .formatted(Formatting.YELLOW)
     );
-    public static final CustomGameRuleCategory KNOCKBACK = new CustomGameRuleCategory(
-        new Identifier("gamerule.category.knockback"),
-        new TranslatableText("gamerule.category.knockback")
+    public static final CustomGameRuleCategory VD_KNOCKBACK = new CustomGameRuleCategory(
+        new Identifier("gamerule.category.vd.knockback"),
+        new TranslatableText("gamerule.category.vd.knockback")
             .formatted(Formatting.BOLD)
             .formatted(Formatting.YELLOW)
     );
--- src/main/java/uk/debb/vanilla_disable/gamerules/RegisterGamerules.java
+++ src/main/java/uk/debb/vanilla_disable/gamerules/RegisterGamerules.java
@@ -43,67 +43,67 @@ public class RegisterGamerules implements ModInitializer {
     @Override
     public void onInitialize() {
         DAMAGE_ENABLED            = GameRuleRegistry.register(
-            "damageEnabled",        CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "damageEnabled",        CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         PROJECTILE_DAMAGE         = GameRuleRegistry.register(
-            "projectileDamage",     CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "projectileDamage",     CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         EXPLOSION_DAMAGE          = GameRuleRegistry.register(
-            "explosionDamage",      CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "explosionDamage",      CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         FALLING_BLOCK_DAMAGE      = GameRuleRegistry.register(
-            "fallingBlockDamage",   CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "fallingBlockDamage",   CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         VOID_DAMAGE               = GameRuleRegistry.register(
-            "voidDamage",           CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "voidDamage",           CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         MAGIC_DAMAGE              = GameRuleRegistry.register(
-            "magicDamage",          CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "magicDamage",          CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         CREATIVE_PLAYER_DAMAGE    = GameRuleRegistry.register(
-            "creativePlayerDamage", CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "creativePlayerDamage", CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         LIGHTNING_DAMAGE          = GameRuleRegistry.register(
-            "lightningDamage",      CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "lightningDamage",      CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         WALL_DAMAGE               = GameRuleRegistry.register(
-            "wallDamage",           CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "wallDamage",           CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         CRAMMING_DAMAGE           = GameRuleRegistry.register(
-            "crammingDamage",       CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "crammingDamage",       CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         STARVATION_DAMAGE         = GameRuleRegistry.register(
-            "starvationDamage",     CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "starvationDamage",     CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         CACTUS_DAMAGE             = GameRuleRegistry.register(
-            "cactusDamage",         CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "cactusDamage",         CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         FLY_INTO_WALL_DAMAGE      = GameRuleRegistry.register(
-            "flyIntoWallDamage",    CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "flyIntoWallDamage",    CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         WITHER_DAMAGE             = GameRuleRegistry.register(
-            "witherDamage",         CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "witherDamage",         CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         ANVIL_DAMAGE              = GameRuleRegistry.register(
-            "anvilDamage",          CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "anvilDamage",          CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         DRAGON_DAMAGE             = GameRuleRegistry.register(
-            "dragonDamage",         CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "dragonDamage",         CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         DRYOUT_DAMAGE             = GameRuleRegistry.register(
-            "dryoutDamage",         CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "dryoutDamage",         CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         SWEET_BERRY_BUSH_DAMAGE   = GameRuleRegistry.register(
-            "sweetBerryBushDamage", CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "sweetBerryBushDamage", CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         FALLING_STALACTITE_DAMAGE = GameRuleRegistry.register(
-            "fallingStalactiteDamage", CreateGameruleCategories.DAMAGE, GameRuleFactory.createBooleanRule(true));
+            "fallingStalactiteDamage", CreateGameruleCategories.VD_DAMAGE, GameRuleFactory.createBooleanRule(true));
         
         KNOCKBACK_ENABLED         = GameRuleRegistry.register(
-            "knockbackEnabled",     CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "knockbackEnabled",     CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         FIREBALL_KNOCKBACK        = GameRuleRegistry.register(
-            "fireballKnockback",    CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "fireballKnockback",    CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         WITHER_SKULL_KNOCKBACK    = GameRuleRegistry.register(
-            "witherSkullKnockback", CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "witherSkullKnockback", CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         DRAGON_KNOCKBACK          = GameRuleRegistry.register(
-            "dragonKnockback",      CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "dragonKnockback",      CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         ARROW_KNOCKBACK           = GameRuleRegistry.register(
-            "arrowKnockback",       CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "arrowKnockback",       CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         TRIDENT_KNOCKBACK         = GameRuleRegistry.register(
-            "tridentKnockback",     CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "tridentKnockback",     CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         LLAMA_SPIT_KNOCKBACK      = GameRuleRegistry.register(
-            "llamaSpitKnockback",   CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "llamaSpitKnockback",   CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         SHULKER_BULLET_KNOCKBACK  = GameRuleRegistry.register(
-            "shulkerBulletKnockback", CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "shulkerBulletKnockback", CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         MOB_ATTACK_KNOCKBACK      = GameRuleRegistry.register(
-            "mobAttackKnockback",   CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "mobAttackKnockback",   CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         PLAYER_ATTACK_KNOCKBACK   = GameRuleRegistry.register(
-            "playerAttackKnockback", CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "playerAttackKnockback", CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         KNOCKBACK_ENCHANTMENT     = GameRuleRegistry.register(
-            "knockbackEnchantment", CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "knockbackEnchantment", CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
         EXPLOSION_KNOCKBACK       = GameRuleRegistry.register(
-            "explosionKnockback",   CreateGameruleCategories.KNOCKBACK, GameRuleFactory.createBooleanRule(true));
+            "explosionKnockback",   CreateGameruleCategories.VD_KNOCKBACK, GameRuleFactory.createBooleanRule(true));
     }
 }
