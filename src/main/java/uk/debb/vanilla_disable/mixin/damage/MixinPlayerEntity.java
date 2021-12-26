eb5ce65e8e9c67bd561bb68d1e71fecf55aabf72
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-26 16:06:15 +0000

    Properly fix global damage gamerule not working

--- src/main/java/uk/debb/vanilla_disable/mixin/damage/MixinPlayerEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/damage/MixinPlayerEntity.java
@@ -22,57 +22,40 @@ public abstract class MixinPlayerEntity extends LivingEntity {
     @Inject(method = "isInvulnerableTo", at = @At(value = "TAIL"), cancellable = true)
     private void isAlsoInvulnerableTo(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
         if (!this.world.getGameRules().getBoolean(RegisterGamerules.DAMAGE_ENABLED)) {
-            cir.setReturnValue(false);
-        }
-        if (damageSource.isProjectile()) {
+            cir.setReturnValue(true);
+        } else if (damageSource.isProjectile()) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.PROJECTILE_DAMAGE));
-        }
-        if (damageSource.isExplosive()) {
+        } else if (damageSource.isExplosive()) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.EXPLOSION_DAMAGE));
-        }
-        if (damageSource.isOutOfWorld()) {
+        } else if (damageSource.isOutOfWorld()) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.VOID_DAMAGE));
-        }
-        if (damageSource.isMagic()) {
+        } else if (damageSource.isMagic()) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.MAGIC_DAMAGE));
-        }
-        if (damageSource.isSourceCreativePlayer()) {
+        } else if (damageSource.isSourceCreativePlayer()) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.CREATIVE_PLAYER_DAMAGE));
-        }
-        if (damageSource == DamageSource.LIGHTNING_BOLT) {
+        } else if (damageSource == DamageSource.LIGHTNING_BOLT) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.LIGHTNING_DAMAGE));
-        }
-        if (damageSource == DamageSource.IN_WALL) {
+        } else if (damageSource == DamageSource.IN_WALL) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.WALL_DAMAGE));
-        }
-        if (damageSource == DamageSource.CRAMMING) {
+        } else if (damageSource == DamageSource.CRAMMING) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.CRAMMING_DAMAGE));
-        }
-        if (damageSource == DamageSource.STARVE) {
+        } else if (damageSource == DamageSource.STARVE) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.STARVATION_DAMAGE));
-        }
-        if (damageSource == DamageSource.CACTUS) {
+        } else if (damageSource == DamageSource.CACTUS) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.CACTUS_DAMAGE));
-        }
-        if (damageSource == DamageSource.FLY_INTO_WALL) {
+        } else if (damageSource == DamageSource.FLY_INTO_WALL) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.FLY_INTO_WALL_DAMAGE));
-        }
-        if (damageSource == DamageSource.WITHER) {
+        } else if (damageSource == DamageSource.WITHER) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.WITHER_DAMAGE));
-        }
-        if (damageSource == DamageSource.ANVIL) {
+        } else if (damageSource == DamageSource.ANVIL) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.ANVIL_DAMAGE));
-        }
-        if (damageSource == DamageSource.DRAGON_BREATH) {
+        } else if (damageSource == DamageSource.DRAGON_BREATH) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.DRAGON_DAMAGE));
-        }
-        if (damageSource == DamageSource.DRYOUT) {
+        } else if (damageSource == DamageSource.DRYOUT) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.DRYOUT_DAMAGE));
-        }
-        if (damageSource == DamageSource.SWEET_BERRY_BUSH) {
+        } else if (damageSource == DamageSource.SWEET_BERRY_BUSH) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.SWEET_BERRY_BUSH_DAMAGE));
-        }
-        if (damageSource == DamageSource.FALLING_STALACTITE) {
+        } else if (damageSource == DamageSource.FALLING_STALACTITE) {
             cir.setReturnValue(!this.world.getGameRules().getBoolean(RegisterGamerules.FALLING_STALACTITE_DAMAGE));
         }
     }
