b38da1e352ee50adecbad0d314b0ae0de8bd2957
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-26 00:33:44 +0000

    Remove unused import

--- src/main/java/uk/debb/vanilla_disable/gamerules/RegisterGamerules.java
+++ src/main/java/uk/debb/vanilla_disable/gamerules/RegisterGamerules.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import uk.debb.vanilla_disable.gamerules.CreateGameruleCategories;
 
 public class RegisterGamerules implements ModInitializer {
     public static GameRules.Key<GameRules.BooleanRule> DAMAGE_ENABLED;
