82e7a77a2b89907238e7669023832cc0becc91d5
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-31 12:14:22 +0000

    Bump to 1.0.1

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -42,7 +42,7 @@ public class Buildscript extends FabricProject {
     @Override
     public String getVersion() {
         // Mod Version
-        return "1.0.0";
+        return "1.0.1";
     }
 
     @Override
