49860965a6a5bb1402215231eef7ddb0226653b2
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-01-02 00:50:13 +0000

    Rewrite effects gamerule code for optimisation and cleaner code

--- src/main/java/uk/debb/vanilla_disable/mixin/effects/MixinLivingEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/effects/MixinLivingEntity.java
@@ -1,5 +1,7 @@
 package uk.debb.vanilla_disable.mixin.effects;
 
+import java.util.HashMap;
+import java.util.Map;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -7,6 +9,7 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,108 +25,64 @@ public abstract class MixinLivingEntity extends Entity {
 
     /**
      * @author DragonEggBedrockBreaking
+     * @reason map of all damage sources to their gamerules
+     */
+    private static final Map<StatusEffect, GameRules.Key<GameRules.BooleanRule>> statusEffectMap = new HashMap<StatusEffect, GameRules.Key<GameRules.BooleanRule>>() {{}};
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason the map otherwise initialises before the gamerules are created and always returns null
+     */
+    private void addOptionsToMap() {
+        statusEffectMap.put(StatusEffects.ABSORPTION, RegisterGamerules.ABSORPTION_EFFECT);
+        statusEffectMap.put(StatusEffects.BAD_OMEN, RegisterGamerules.BAD_OMEN_EFFECT);
+        statusEffectMap.put(StatusEffects.BLINDNESS, RegisterGamerules.BLINDNESS_EFFECT);
+        statusEffectMap.put(StatusEffects.CONDUIT_POWER, RegisterGamerules.CONDUIT_POWER_EFFECT);
+        statusEffectMap.put(StatusEffects.DOLPHINS_GRACE, RegisterGamerules.DOLPHINS_GRACE_EFFECT);
+        statusEffectMap.put(StatusEffects.FIRE_RESISTANCE, RegisterGamerules.FIRE_RESISTANCE_EFFECT);
+        statusEffectMap.put(StatusEffects.GLOWING, RegisterGamerules.GLOWING_EFFECT);
+        statusEffectMap.put(StatusEffects.HASTE, RegisterGamerules.HASTE_EFFECT);
+        statusEffectMap.put(StatusEffects.HEALTH_BOOST, RegisterGamerules.HEALTH_BOOST_EFFECT);
+        statusEffectMap.put(StatusEffects.HUNGER, RegisterGamerules.HUNGER_EFFECT);
+        statusEffectMap.put(StatusEffects.INSTANT_DAMAGE, RegisterGamerules.INSTANT_DAMAGE_EFFECT);
+        statusEffectMap.put(StatusEffects.INSTANT_HEALTH, RegisterGamerules.INSTANT_HEALTH_EFFECT);
+        statusEffectMap.put(StatusEffects.INVISIBILITY, RegisterGamerules.INVISIBILITY_EFFECT);
+        statusEffectMap.put(StatusEffects.JUMP_BOOST, RegisterGamerules.JUMP_BOOST_EFFECT);
+        statusEffectMap.put(StatusEffects.LEVITATION, RegisterGamerules.LEVITATION_EFFECT);
+        statusEffectMap.put(StatusEffects.LUCK, RegisterGamerules.LUCK_EFFECT);
+        statusEffectMap.put(StatusEffects.MINING_FATIGUE, RegisterGamerules.MINING_FATIGUE_EFFECT);
+        statusEffectMap.put(StatusEffects.NAUSEA, RegisterGamerules.NAUSEA_EFFECT);
+        statusEffectMap.put(StatusEffects.NIGHT_VISION, RegisterGamerules.NIGHT_VISION_EFFECT);
+        statusEffectMap.put(StatusEffects.POISON, RegisterGamerules.POISON_EFFECT);
+        statusEffectMap.put(StatusEffects.REGENERATION, RegisterGamerules.REGENERATION_EFFECT);
+        statusEffectMap.put(StatusEffects.RESISTANCE, RegisterGamerules.RESISTANCE_EFFECT);
+        statusEffectMap.put(StatusEffects.SATURATION, RegisterGamerules.SATURATION_EFFECT);
+        statusEffectMap.put(StatusEffects.SLOWNESS, RegisterGamerules.SLOWNESS_EFFECT);
+        statusEffectMap.put(StatusEffects.SLOW_FALLING, RegisterGamerules.SLOW_FALLING_EFFECT);
+        statusEffectMap.put(StatusEffects.SPEED, RegisterGamerules.SPEED_EFFECT);
+        statusEffectMap.put(StatusEffects.STRENGTH, RegisterGamerules.STRENGTH_EFFECT);
+        statusEffectMap.put(StatusEffects.UNLUCK, RegisterGamerules.UNLUCK_EFFECT);
+        statusEffectMap.put(StatusEffects.WATER_BREATHING, RegisterGamerules.WATER_BREATHING_EFFECT);
+        statusEffectMap.put(StatusEffects.WEAKNESS, RegisterGamerules.WEAKNESS_EFFECT);
+        statusEffectMap.put(StatusEffects.WITHER, RegisterGamerules.WITHER_EFFECT);
+    }
+
+    /**
+     * @author DragonEggBedrockBreaking
      * @reason disable vanilla effects
      * @param effect the status effect
      * @param cir the returnable callback info
      */
     @Inject(method = "canHaveStatusEffect", at = @At("HEAD"), cancellable = true)
     private void canItHaveStatusEffect(StatusEffectInstance effect, CallbackInfoReturnable<Boolean> cir) {
-        StatusEffect statusEffect = effect.getEffectType();
         if (((Object) this) instanceof ServerPlayerEntity) {
-            if (!this.world.getGameRules().getBoolean(RegisterGamerules.EFFECTS_ENABLED)) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.ABSORPTION_EFFECT) &&
-                statusEffect == StatusEffects.ABSORPTION) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.BAD_OMEN_EFFECT) &&
-                statusEffect == StatusEffects.BAD_OMEN) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.BLINDNESS_EFFECT) &&
-                statusEffect == StatusEffects.BLINDNESS) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.CONDUIT_POWER_EFFECT) &&
-                statusEffect == StatusEffects.CONDUIT_POWER) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.DOLPHINS_GRACE_EFFECT) &&
-                statusEffect == StatusEffects.DOLPHINS_GRACE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.FIRE_RESISTANCE_EFFECT) &&
-                statusEffect == StatusEffects.FIRE_RESISTANCE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.GLOWING_EFFECT) &&
-                statusEffect == StatusEffects.GLOWING) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.HASTE_EFFECT) &&
-                statusEffect == StatusEffects.HASTE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.HEALTH_BOOST_EFFECT) &&
-                statusEffect == StatusEffects.HEALTH_BOOST) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.HUNGER_EFFECT) &&
-                statusEffect == StatusEffects.HUNGER) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.INSTANT_DAMAGE_EFFECT) &&
-                statusEffect == StatusEffects.INSTANT_DAMAGE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.INSTANT_HEALTH_EFFECT) &&
-                statusEffect == StatusEffects.INSTANT_HEALTH) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.INVISIBILITY_EFFECT) &&
-                statusEffect == StatusEffects.INVISIBILITY) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.JUMP_BOOST_EFFECT) &&
-                statusEffect == StatusEffects.JUMP_BOOST) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.LEVITATION_EFFECT) &&
-                statusEffect == StatusEffects.LEVITATION) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.LUCK_EFFECT) &&
-                statusEffect == StatusEffects.LUCK) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.MINING_FATIGUE_EFFECT) &&
-                statusEffect == StatusEffects.MINING_FATIGUE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.NAUSEA_EFFECT) &&
-                statusEffect == StatusEffects.NAUSEA) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.NIGHT_VISION_EFFECT) &&
-                statusEffect == StatusEffects.NIGHT_VISION) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.POISON_EFFECT) &&
-                statusEffect == StatusEffects.POISON) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.REGENERATION_EFFECT) &&
-                statusEffect == StatusEffects.REGENERATION) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.RESISTANCE_EFFECT) &&
-                statusEffect == StatusEffects.RESISTANCE) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.SATURATION_EFFECT) &&
-                statusEffect == StatusEffects.SATURATION) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.SLOWNESS_EFFECT) &&
-                statusEffect == StatusEffects.SLOWNESS) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.SLOW_FALLING_EFFECT) &&
-                statusEffect == StatusEffects.SLOW_FALLING) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.SPEED_EFFECT) &&
-                statusEffect == StatusEffects.SPEED) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.STRENGTH_EFFECT) &&
-                statusEffect == StatusEffects.STRENGTH) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.UNLUCK_EFFECT) &&
-                statusEffect == StatusEffects.UNLUCK) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.WATER_BREATHING_EFFECT) &&
-                statusEffect == StatusEffects.WATER_BREATHING) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.WEAKNESS_EFFECT) &&
-                statusEffect == StatusEffects.WEAKNESS) {
-                cir.setReturnValue(false);
-            } else if (!this.world.getGameRules().getBoolean(RegisterGamerules.WITHER_EFFECT) &&
-                statusEffect == StatusEffects.WITHER) {
+            StatusEffect statusEffect = effect.getEffectType();
+            if (statusEffectMap.isEmpty()) {
+                this.addOptionsToMap();
+            }
+            GameRules.Key<GameRules.BooleanRule> effectGamerule = statusEffectMap.get(statusEffect);
+            if ((!this.world.getGameRules().getBoolean(RegisterGamerules.EFFECTS_ENABLED)) ||
+                (effectGamerule != null && !this.world.getGameRules().getBoolean(effectGamerule))) {
                 cir.setReturnValue(false);
             }
         }
