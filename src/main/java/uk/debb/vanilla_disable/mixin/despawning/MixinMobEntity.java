3cecae9ac13177e82363a90a32080581d98608f8
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-01-11 20:31:36 +0000

    Rewrite despawning gamerule code for optimisation and cleaner code

--- src/main/java/uk/debb/vanilla_disable/mixin/despawning/MixinMobEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/despawning/MixinMobEntity.java
@@ -1,5 +1,7 @@
 package uk.debb.vanilla_disable.mixin.despawning;
 
+import java.util.HashMap;
+import java.util.Map;
 import net.minecraft.entity.Bucketable;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -11,16 +13,16 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.AxolotlEntity;
 import net.minecraft.entity.passive.FishEntity;
 import net.minecraft.entity.passive.GlowSquidEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 
-@Mixin(value = MobEntity.class, priority = 999)
+@Mixin(MobEntity.class)
 public abstract class MixinMobEntity extends LivingEntity implements Bucketable {
     protected MixinMobEntity(EntityType<? extends MobEntity> entityType, World world) {
         super((EntityType<? extends LivingEntity>)entityType, world);
@@ -30,98 +32,32 @@ public abstract class MixinMobEntity extends LivingEntity implements Bucketable 
 
     /**
      * @author DragonEggBedrockBreaking
-     * @reason Change the immediate despawn range based on gamerules
-     * @return The immediate despawn range
-     */    
-    private int getImmediateDespawnRange() {
-        MobEntity entity = (MobEntity) (Object) this;
-        GameRules gamerules = this.getWorld().getGameRules();
-        if (entity instanceof HostileEntity) {
-            return gamerules.getInt(RegisterGamerules.MONSTER_MAX_DESPAWN);
-        }
-        if (entity instanceof AnimalEntity) {
-            return gamerules.getInt(RegisterGamerules.CREATURE_MAX_DESPAWN);
-        }
-        if (entity instanceof AmbientEntity) {
-            return gamerules.getInt(RegisterGamerules.AMBIENT_MAX_DESPAWN);
-        }
-        if (entity instanceof AxolotlEntity) {
-            return gamerules.getInt(RegisterGamerules.AXOLOTL_MAX_DESPAWN);
-        }
-        if (entity instanceof GlowSquidEntity) {
-            return gamerules.getInt(RegisterGamerules.GLOWSQUID_MAX_DESPAWN);
-        }
-        if (entity instanceof FishEntity) {
-            return gamerules.getInt(RegisterGamerules.WATER_AMBIENT_MAX_DESPAWN);
-        }
-        if (entity instanceof WaterCreatureEntity) {
-            return gamerules.getInt(RegisterGamerules.WATER_CREATURE_MAX_DESPAWN);
-        }
-        return 128;
-    }
+     * @reason map of many mob groups to their gamerules
+     */
+    private static final Map<Class<?>, GameRules.Key<GameRules.BooleanRule>> spawnGroupDespawnMap = new HashMap<Class<?>, GameRules.Key<GameRules.BooleanRule>>();
 
     /**
      * @author DragonEggBedrockBreaking
-     * @reason Change the range where mobs can despawn based on gamerules
-     * @return The range where mobs can despawn
+     * @reason the map otherwise initialises before the gamerules are created and always returns null
      */
-    private int getDespawnStartRange() {
-        MobEntity entity = (MobEntity) (Object) this;
-        GameRules gamerules = this.getWorld().getGameRules();
-        if (entity instanceof HostileEntity) {
-            return gamerules.getInt(RegisterGamerules.MONSTER_MIN_DESPAWN);
-        }
-        if (entity instanceof AnimalEntity) {
-            return gamerules.getInt(RegisterGamerules.CREATURE_MIN_DESPAWN);
-        }
-        if (entity instanceof AmbientEntity) {
-            return gamerules.getInt(RegisterGamerules.AMBIENT_MIN_DESPAWN);
-        }
-        if (entity instanceof AxolotlEntity) {
-            return gamerules.getInt(RegisterGamerules.AXOLOTL_MIN_DESPAWN);
-        }
-        if (entity instanceof GlowSquidEntity) {
-            return gamerules.getInt(RegisterGamerules.GLOWSQUID_MIN_DESPAWN);
-        }
-        if (entity instanceof FishEntity) {
-            return gamerules.getInt(RegisterGamerules.WATER_AMBIENT_MIN_DESPAWN);
-        }
-        if (entity instanceof WaterCreatureEntity) {
-            return gamerules.getInt(RegisterGamerules.WATER_CREATURE_MIN_DESPAWN);
-        }
-        return 32;
+    private void addOptionsToMap() {
+        spawnGroupDespawnMap.put(HostileEntity.class, RegisterGamerules.MONSTERS_DESPAWN);
+        spawnGroupDespawnMap.put(AnimalEntity.class, RegisterGamerules.CREATURES_DESPAWN);
+        spawnGroupDespawnMap.put(AmbientEntity.class, RegisterGamerules.AMBIENT_DESPAWN);
+        spawnGroupDespawnMap.put(AxolotlEntity.class, RegisterGamerules.AXOLOTLS_DESPAWN);
+        spawnGroupDespawnMap.put(GlowSquidEntity.class, RegisterGamerules.GLOWSQUIDS_DESPAWN);
+        spawnGroupDespawnMap.put(FishEntity.class, RegisterGamerules.WATER_AMBIENT_DESPAWN);
+        spawnGroupDespawnMap.put(WaterCreatureEntity.class, RegisterGamerules.WATER_CREATURES_DESPAWN);
     }
-
     /**
      * @author DragonEggBedrockBreaking
-     * @reason Change whether the mob can despawn based on gamerules
-     * @return Whether the mob can despawn
+     * @reason Fish and axolotls have additional restrictions
+     * @return Whether the additional restrictions are met
      */
-    private boolean canDespawn() {
+    private boolean additionalRestrictionsMet() {
         MobEntity entity = (MobEntity) (Object) this;
-        GameRules gamerules = this.getWorld().getGameRules();
-        if (entity instanceof HostileEntity) {
-            return gamerules.getBoolean(RegisterGamerules.MONSTERS_DESPAWN);
-        }
-        if (entity instanceof AnimalEntity) {
-            return gamerules.getBoolean(RegisterGamerules.CREATURES_DESPAWN);
-        }
-        if (entity instanceof AmbientEntity) {
-            return gamerules.getBoolean(RegisterGamerules.AMBIENT_DESPAWN);
-        }
-        if (entity instanceof AxolotlEntity) {
-            return gamerules.getBoolean(RegisterGamerules.AXOLOTLS_DESPAWN) &&
-                   !this.hasCustomName() && !this.isFromBucket();
-        }
-        if (entity instanceof GlowSquidEntity) {
-            return gamerules.getBoolean(RegisterGamerules.GLOWSQUIDS_DESPAWN);
-        }
-        if (entity instanceof FishEntity) {
-            return gamerules.getBoolean(RegisterGamerules.WATER_AMBIENT_DESPAWN) &&
-                   !this.hasCustomName() && !this.isFromBucket();
-        }
-        if (entity instanceof WaterCreatureEntity) {
-            return gamerules.getBoolean(RegisterGamerules.WATER_CREATURES_DESPAWN);
+        if (entity instanceof AxolotlEntity || entity instanceof FishEntity) {
+            return !this.hasCustomName() && !this.isFromBucket();
         }
         return true;
     }
@@ -130,31 +66,14 @@ public abstract class MixinMobEntity extends LivingEntity implements Bucketable 
      * @author DragonEggBedrockBreaking
      * @reason Change whether the mob despawns based on gamerules
      */
-    @Overwrite
-    public void checkDespawn() {
-        MobEntity entity = (MobEntity) (Object) this;
-        if (this.world.getDifficulty() == Difficulty.PEACEFUL && entity instanceof HostileEntity) {
-            this.discard();
-            return;
+    @Inject(method = "canImmediatelyDespawn", at = @At("HEAD"), cancellable = true)
+    private void cannotImmediatelyDespawn(double distanceSquared, CallbackInfoReturnable<Boolean> cir) {
+        if (spawnGroupDespawnMap.isEmpty()) {
+            this.addOptionsToMap();
         }
-        if (this.persistent || this.hasVehicle()) {
-            this.despawnCounter = 0;
-            return;
-        }
-        PlayerEntity player = this.world.getClosestPlayer(this, -1.0);
-        if (player != null) {
-            int i;
-            double d = player.squaredDistanceTo(this);
-            if (d > (double)((i = this.getImmediateDespawnRange()) * i) && this.canDespawn()) {
-                this.discard();
-            }
-            int k = this.getDespawnStartRange();
-            int l = k * k;
-            if (this.despawnCounter > 600 && this.random.nextInt(800) == 0 && d > (double)l && this.canDespawn()) {
-                this.discard();
-            } else if (d < (double)l) {
-                this.despawnCounter = 0;
-            }
+        GameRules.Key<GameRules.BooleanRule> gameRule = spawnGroupDespawnMap.get(this.getClass());
+        if (gameRule != null && RegisterGamerules.getServer().getGameRules().getBoolean(gameRule)) {
+            cir.setReturnValue(this.additionalRestrictionsMet());
         }
     }
 }
---
+++ src/main/java/uk/debb/vanilla_disable/mixin/despawning/MixinSpawnGroup.java
@@ -0,0 +1,76 @@
+package uk.debb.vanilla_disable.mixin.despawning;
+
+import java.util.HashMap;
+import java.util.Map;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.GameRules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
+
+@Mixin(SpawnGroup.class)
+public abstract class MixinSpawnGroup {
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason map of many mob groups to their gamerules
+     */
+    private static final Map<SpawnGroup, GameRules.Key<GameRules.IntRule>> spawnGroupImmediateMap = new HashMap<SpawnGroup, GameRules.Key<GameRules.IntRule>>();
+    private static final Map<SpawnGroup, GameRules.Key<GameRules.IntRule>> spawnGroupStartMap = new HashMap<SpawnGroup, GameRules.Key<GameRules.IntRule>>();
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason the map otherwise initialises before the gamerules are created and always returns null
+     */
+    private void addImmediateOptionsToMap() {
+        spawnGroupImmediateMap.put(SpawnGroup.MONSTER, RegisterGamerules.MONSTER_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.CREATURE, RegisterGamerules.CREATURE_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.AMBIENT, RegisterGamerules.AMBIENT_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.AXOLOTLS, RegisterGamerules.AXOLOTL_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.UNDERGROUND_WATER_CREATURE, RegisterGamerules.GLOWSQUID_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.WATER_AMBIENT, RegisterGamerules.WATER_AMBIENT_MAX_DESPAWN);
+        spawnGroupImmediateMap.put(SpawnGroup.WATER_CREATURE, RegisterGamerules.WATER_CREATURE_MIN_DESPAWN);
+    }
+    private void addStartOptionsToMap() {
+        spawnGroupStartMap.put(SpawnGroup.MONSTER, RegisterGamerules.MONSTER_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.CREATURE, RegisterGamerules.CREATURE_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.AMBIENT, RegisterGamerules.AMBIENT_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.AXOLOTLS, RegisterGamerules.AXOLOTL_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.UNDERGROUND_WATER_CREATURE, RegisterGamerules.GLOWSQUID_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.WATER_AMBIENT, RegisterGamerules.WATER_AMBIENT_MIN_DESPAWN);
+        spawnGroupStartMap.put(SpawnGroup.WATER_CREATURE, RegisterGamerules.WATER_CREATURE_MAX_DESPAWN);
+    }
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason edit immediate despawn range
+     * @param cir returnable callback info
+     */
+    @Inject(method = "getImmediateDespawnRange", at = @At("HEAD"), cancellable = true)
+    public void editImmediateDespawnRange(CallbackInfoReturnable<Integer> cir) {
+        if (spawnGroupImmediateMap.isEmpty()) {
+            this.addImmediateOptionsToMap();
+        }
+       GameRules.Key<GameRules.IntRule> gameRule = spawnGroupImmediateMap.get((SpawnGroup) (Object) this);
+       if (gameRule != null) {
+           cir.setReturnValue(RegisterGamerules.getServer().getGameRules().getInt(gameRule));
+       }
+    }
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason edit immediate despawn range
+     * @param cir returnable callback info
+     */
+    @Inject(method = "getDespawnStartRange", at = @At("HEAD"), cancellable = true)
+    public void editDespawnStartRange(CallbackInfoReturnable<Integer> cir) {
+        if (spawnGroupStartMap.isEmpty()) {
+            this.addStartOptionsToMap();
+        }
+        GameRules.Key<GameRules.IntRule> gameRule = spawnGroupStartMap.get((SpawnGroup) (Object) this);
+        if (gameRule != null) {
+            cir.setReturnValue(RegisterGamerules.getServer().getGameRules().getInt(gameRule));
+        }
+    }
+}
