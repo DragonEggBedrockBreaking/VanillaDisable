dc3ce11815367a9103dad40256693e7552c93878
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-01-12 18:35:30 +0000

    Rewrite mob spawning gamerule code for optimisation and cleaner code

--- src/main/java/uk/debb/vanilla_disable/mixin/spawning/MixinSpawnHelper.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/spawning/MixinSpawnHelper.java
@@ -1,7 +1,10 @@
 package uk.debb.vanilla_disable.mixin.spawning;
 
+import java.util.HashMap;
+import java.util.Map;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.SpawnHelper;
 import net.minecraft.world.SpawnHelper.Checker;
 import net.minecraft.world.SpawnHelper.Runner;
@@ -16,6 +19,26 @@ import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 public class MixinSpawnHelper {
     /**
      * @author DragonEggBedrockBreaking
+     * @reason map of all spawn groups to their gamerules
+     */
+    private static final Map<SpawnGroup, GameRules.Key<GameRules.BooleanRule>> spawnGroupMap = new HashMap<SpawnGroup, GameRules.Key<GameRules.BooleanRule>>();
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason the map otherwise initialises before the gamerules are created and always returns null
+     */
+    private static void addOptionsToMap() {
+        spawnGroupMap.put(SpawnGroup.MONSTER, RegisterGamerules.MONSTER_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.CREATURE, RegisterGamerules.CREATURE_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.AMBIENT, RegisterGamerules.AMBIENT_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.AXOLOTLS, RegisterGamerules.AXOLOTL_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.UNDERGROUND_WATER_CREATURE, RegisterGamerules.GLOWSQUID_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.WATER_AMBIENT, RegisterGamerules.WATER_AMBIENT_SPAWNING);
+        spawnGroupMap.put(SpawnGroup.WATER_CREATURE, RegisterGamerules.WATER_CREATURE_SPAWNING);
+    }
+
+    /**
+     * @author DragonEggBedrockBreaking
      * @param group the type of entity
      * @param world the world
      * @param chunk the chunk
@@ -25,32 +48,11 @@ public class MixinSpawnHelper {
      */
     @Inject(method = "spawnEntitiesInChunk", at = @At(value = "HEAD"), cancellable = true)
     private static void cancelSpawningEntitiesInChunk(SpawnGroup group, ServerWorld world, WorldChunk chunk, Checker checker, Runner runner, CallbackInfo ci) {
-        if (group == SpawnGroup.MONSTER &&
-            !world.getGameRules().getBoolean(RegisterGamerules.MONSTER_SPAWNING)) {
-            ci.cancel();
+        if (spawnGroupMap.isEmpty()) {
+            addOptionsToMap();
         }
-        if (group == SpawnGroup.CREATURE &&
-            !world.getGameRules().getBoolean(RegisterGamerules.CREATURE_SPAWNING)) {
-            ci.cancel();
-        }
-        if (group == SpawnGroup.AMBIENT &&
-            !world.getGameRules().getBoolean(RegisterGamerules.AMBIENT_SPAWNING)) {
-            ci.cancel();
-        }
-        if (group == SpawnGroup.AXOLOTLS &&
-            !world.getGameRules().getBoolean(RegisterGamerules.AXOLOTL_SPAWNING)) {
-            ci.cancel();
-        }
-        if (group == SpawnGroup.UNDERGROUND_WATER_CREATURE &&
-            !world.getGameRules().getBoolean(RegisterGamerules.GLOWSQUID_SPAWNING)) {
-            ci.cancel();
-        }
-        if (group == SpawnGroup.WATER_CREATURE &&
-            !world.getGameRules().getBoolean(RegisterGamerules.WATER_CREATURE_SPAWNING)) {
-            ci.cancel();
-        }
-        if (group == SpawnGroup.WATER_AMBIENT &&
-            !world.getGameRules().getBoolean(RegisterGamerules.WATER_AMBIENT_SPAWNING)) {
+        GameRules.Key<GameRules.BooleanRule> gameRule = spawnGroupMap.get(group);
+        if (gameRule != null && !world.getGameRules().getBoolean(gameRule)) {
             ci.cancel();
         }
     }
