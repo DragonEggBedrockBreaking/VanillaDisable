39b38f5017619f5a4282839cd373015d3bf40f7a
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-01-12 19:02:47 +0000

    Move around some imports

--- src/main/java/uk/debb/vanilla_disable/mixin/fluids/MixinBucketItem.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/fluids/MixinBucketItem.java
@@ -21,7 +21,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 
 @Mixin(BucketItem.class)
--- src/main/java/uk/debb/vanilla_disable/mixin/misc/MixinEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/misc/MixinEntity.java
@@ -2,13 +2,12 @@ package uk.debb.vanilla_disable.mixin.misc;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
-import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
