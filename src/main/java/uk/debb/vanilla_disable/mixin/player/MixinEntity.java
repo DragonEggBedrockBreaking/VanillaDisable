ab48f214e2937ea89d45171af125a78e70c82612
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-02-19 03:38:24 +0000

    Fix weird crouching issues

--- src/main/java/uk/debb/vanilla_disable/mixin/player/MixinEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/player/MixinEntity.java
@@ -1,10 +1,8 @@
 package uk.debb.vanilla_disable.mixin.player;
 
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -12,8 +10,6 @@ import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
-    @Shadow public abstract void setPose(Pose pose);
-
     /**
      * @author DragonEggBedrockBreaking
      * @reason stop players from being set on fire
@@ -57,7 +53,6 @@ public abstract class MixinEntity {
      */
     @Inject(method = "isSwimming", at = @At("HEAD"), cancellable = true)
     private void cannotSwim(CallbackInfoReturnable<Boolean> cir) {
-        this.setPose(Pose.STANDING);
         if ((Object)this instanceof Player && !RegisterGamerules.getServer().getGameRules().getBoolean(RegisterGamerules.PLAYER_CAN_SWIM)) {
             cir.setReturnValue(false);
         }
