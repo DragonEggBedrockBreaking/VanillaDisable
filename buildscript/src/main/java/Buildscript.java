9ec1ebdbc6911ab36cce0174466fffecc1dd390a
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-02-19 02:18:53 +0000

    Use Fabric Loader 0.13.2 in buildscript, and require 0.13.1

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -27,7 +27,7 @@ public class Buildscript extends FabricProject {
     @Override
     public FabricLoader getLoader() {
         // Fabric Loader Version
-        return new FabricLoader(FabricMaven.URL, FabricMaven.loader("0.12.12"));
+        return new FabricLoader(FabricMaven.URL, FabricMaven.loader("0.13.2"));
     }
 
     @Override
