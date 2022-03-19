d64499030413f55f84353f547620dfb9a28c81dd
DragonEggBedrockBreaking <cuber3million@gmail.com>
2022-03-19 12:28:20 +0000

    Replace `@Overwrite` with 2 `@ModifyArg`s

--- src/main/java/uk/debb/vanilla_disable/mixin/mob/MixinPig.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/mob/MixinPig.java
@@ -1,27 +1,18 @@
 package uk.debb.vanilla_disable.mixin.mob;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.goal.BreedGoal;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
-import net.minecraft.world.entity.ai.goal.FollowParentGoal;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
-import net.minecraft.world.entity.ai.goal.PanicGoal;
-import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Pig;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.debb.vanilla_disable.gamerules.RegisterGamerules;
 
@@ -30,8 +21,6 @@ public abstract class MixinPig extends Animal {
     public MixinPig(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
     }
-
-    @Shadow @Final private static Ingredient FOOD_ITEMS;
 
     /**
      * @author DragonEggBedrockBreaking
@@ -49,23 +38,47 @@ public abstract class MixinPig extends Animal {
 
     /**
      * @author DragonEggBedrockBreaking
-     * @reason change the item that the pig is attracted to
+     * @reason stop pigs being attracted to carrots on sticks
+     * @param goal the original goal
+     * @return the goal to go to wheat instead
      */
-    @Overwrite
-    public void registerGoals() {
-        if (RegisterGamerules.getServer() == null) return;
-        this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(1, new PanicGoal(this, 1.25));
-        this.goalSelector.addGoal(3, new BreedGoal(this, 1.0));
+    @ModifyArg(
+        method = "registerGoals",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/ai/goal/GoalSelector;addGoal(ILnet/minecraft/world/entity/ai/goal/Goal;)V",
+            ordinal = 3
+        ),
+        index = 1
+    )
+    private Goal modifyCarrotOnStickGoal(Goal goal) {
+        if (RegisterGamerules.getServer() == null) return goal;
         if (RegisterGamerules.getServer().getGameRules().getBoolean(RegisterGamerules.PIGS_BREED_WITH_WHEAT)) {
-            this.goalSelector.addGoal(4, new TemptGoal(this, 1.2, Ingredient.of(Items.WHEAT), false));
-        } else {
-            this.goalSelector.addGoal(4, new TemptGoal(this, 1.2, Ingredient.of(Items.CARROT_ON_A_STICK), false));
-            this.goalSelector.addGoal(4, new TemptGoal(this, 1.2, MixinPig.FOOD_ITEMS, false));
+            return new TemptGoal(this, 1.2D, Ingredient.of(Items.WHEAT), false);
         }
-        this.goalSelector.addGoal(5, new FollowParentGoal(this, 1.1));
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0));
-        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 6.0F));
-        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        return goal;
+    }
+
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason stop pigs being attracted to carrots, potatoes, and beetroot
+     * @param goal the original goal
+     * @return the goal to go to wheat instead
+     */
+    @ModifyArg(
+        method = "registerGoals",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/ai/goal/GoalSelector;addGoal(ILnet/minecraft/world/entity/ai/goal/Goal;)V",
+            ordinal = 4
+        ),
+        index = 1
+    )
+    private Goal modifyBreedItemsGoal(Goal goal) {
+        if (RegisterGamerules.getServer() == null) return goal;
+        if (RegisterGamerules.getServer().getGameRules().getBoolean(RegisterGamerules.PIGS_BREED_WITH_WHEAT)) {
+            return new TemptGoal(this, 1.2D, Ingredient.of(Items.WHEAT), false);
+        }
+        return goal;
     }
 }
