446f4b8508bbb49964298f4ef9d02eda06ff92c2
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-05-24 13:35:36 +0100

    Make our functions @Unique

--- src/main/java/uk/debb/vanilla_disable/util/VDLogger.java
+++ src/main/java/uk/debb/vanilla_disable/util/VDLogger.java
@@ -2,11 +2,12 @@ package uk.debb.vanilla_disable.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Unique;
 
 public class VDLogger {
     // Creates a logger for this mod to use
     private static Logger logger = LoggerFactory.getLogger("Vanilla Disable");
-    public static Logger getLogger() {
+    @Unique public static Logger getLogger() {
         return logger;
     }
 }
--- src/main/java/uk/debb/vanilla_disable/util/VDServer.java
+++ src/main/java/uk/debb/vanilla_disable/util/VDServer.java
@@ -1,14 +1,15 @@
 package uk.debb.vanilla_disable.util;
 
 import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Unique;
 
 public class VDServer {
     // Defining the minecraft server that I can use to get gamerules anywhere
     private static MinecraftServer server;
-    public static MinecraftServer getServer() {
+    @Unique public static MinecraftServer getServer() {
         return server;
     }
-    public static void setServer(MinecraftServer serverArg) {
+    @Unique public static void setServer(MinecraftServer serverArg) {
         VDServer.server = serverArg;
     }
 }
