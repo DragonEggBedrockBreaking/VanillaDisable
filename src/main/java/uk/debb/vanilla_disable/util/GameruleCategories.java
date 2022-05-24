94d2c0cf33d00b68921981e1430a193c1fced10f
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-05-24 15:29:23 +0100

    Add a helper function to make the registration of gamerule categories easier

--- src/main/java/uk/debb/vanilla_disable/util/GameruleCategories.java
+++ src/main/java/uk/debb/vanilla_disable/util/GameruleCategories.java
@@ -4,90 +4,37 @@ import net.fabricmc.fabric.api.gamerule.v1.CustomGameRuleCategory;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Unique;
 
 public class GameruleCategories {
-    public static final CustomGameRuleCategory VD_DAMAGE = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.damage"),
-        new TranslatableComponent("gamerule.category.vd.damage")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_KNOCKBACK = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.knockback"),
-        new TranslatableComponent("gamerule.category.vd.knockback")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_SPAWNING = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.spawning"),
-        new TranslatableComponent("gamerule.category.vd.spawning")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_DESPAWNING = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.despawning"),
-        new TranslatableComponent("gamerule.category.vd.despawning")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_SPAWN_LIMITS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.spawn_limits"),
-        new TranslatableComponent("gamerule.category.vd.spawn_limits")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_COMMANDS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.commands"),
-        new TranslatableComponent("gamerule.category.vd.commands")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_FLUIDS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.fluids"),
-        new TranslatableComponent("gamerule.category.vd.fluids")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_MOBS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.mobs"),
-        new TranslatableComponent("gamerule.category.vd.mobs")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_EFFECTS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.effects"),
-        new TranslatableComponent("gamerule.category.vd.effects")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_ENCHANTMENTS = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.enchantments"),
-        new TranslatableComponent("gamerule.category.vd.enchantments")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_WORLDGEN = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.worldgen"),
-        new TranslatableComponent("gamerule.category.vd.worldgen")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_PLAYER = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.player"),
-        new TranslatableComponent("gamerule.category.vd.player")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_REDSTONE = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.redstone"),
-        new TranslatableComponent("gamerule.category.vd.redstone")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
-    public static final CustomGameRuleCategory VD_MISC = new CustomGameRuleCategory(
-        new ResourceLocation("gamerule.category.vd.misc"),
-        new TranslatableComponent("gamerule.category.vd.misc")
-            .withStyle(ChatFormatting.BOLD)
-            .withStyle(ChatFormatting.DARK_GREEN)
-    );
+    /**
+     * @author DragonEggBedrockBreaking
+     * @reason helper function to register each gamerule category
+     * @param identifier the id for the gamerule category
+     * @return the gamerule category
+     */
+    @Unique
+    private static CustomGameRuleCategory createCustomGameRuleCategory(String identifier) {
+        return new CustomGameRuleCategory(
+            new ResourceLocation(identifier),
+            new TranslatableComponent(identifier)
+                .withStyle(ChatFormatting.BOLD)
+                .withStyle(ChatFormatting.DARK_GREEN)
+        );
+    }
+
+    public static final CustomGameRuleCategory VD_DAMAGE = createCustomGameRuleCategory("gamerule.category.vd.damage");
+    public static final CustomGameRuleCategory VD_KNOCKBACK = createCustomGameRuleCategory("gamerule.category.vd.knockback");
+    public static final CustomGameRuleCategory VD_SPAWNING = createCustomGameRuleCategory("gamerule.category.vd.spawning");
+    public static final CustomGameRuleCategory VD_DESPAWNING = createCustomGameRuleCategory("gamerule.category.vd.despawning");
+    public static final CustomGameRuleCategory VD_SPAWN_LIMITS = createCustomGameRuleCategory("gamerule.category.vd.spawn_limits");
+    public static final CustomGameRuleCategory VD_COMMANDS = createCustomGameRuleCategory("gamerule.category.vd.commands");
+    public static final CustomGameRuleCategory VD_FLUIDS = createCustomGameRuleCategory("gamerule.category.vd.fluids");
+    public static final CustomGameRuleCategory VD_MOBS = createCustomGameRuleCategory("gamerule.category.vd.mods");
+    public static final CustomGameRuleCategory VD_EFFECTS = createCustomGameRuleCategory("gamerule.category.vd.effects");
+    public static final CustomGameRuleCategory VD_ENCHANTMENTS = createCustomGameRuleCategory("gamerule.category.vd.enchantments");
+    public static final CustomGameRuleCategory VD_WORLDGEN = createCustomGameRuleCategory("gamerule.category.vd.worldgen");
+    public static final CustomGameRuleCategory VD_PLAYER = createCustomGameRuleCategory("gamerule.category.vd.player");
+    public static final CustomGameRuleCategory VD_REDSTONE = createCustomGameRuleCategory("gamerule.category.vd.redstone");
+    public static final CustomGameRuleCategory VD_MISC = createCustomGameRuleCategory("gamerule.category.vd.misc");
 }
