e86aee9a12c2f8372a8ba0f6decd06c84fa3e53f
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-05-27 20:46:32 +0100

    Rewrite redstone wire gamerule to make it work properly

--- src/main/java/uk/debb/vanilla_disable/mixin/redstone/MixinRedStoneWireBlock.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/redstone/MixinRedStoneWireBlock.java
@@ -1,8 +1,5 @@
 package uk.debb.vanilla_disable.mixin.redstone;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.RedStoneWireBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,29 +16,10 @@ public abstract class MixinRedStoneWireBlock {
      * @author DragonEggBedrockBreaking
      * @reason modify the signal outputted
      * @param blockState the state of the block
-     * @param blockGetter the block getter
-     * @param blockPos the position of the block
-     * @param direction the direction of the block
      * @param cir the returnable callback info (Integer)
      */
-    @Inject(method = "getSignal", at = @At("HEAD"), cancellable = true)
-    private void modifySignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, CallbackInfoReturnable<Integer> cir) {
-        if (VDServer.getServer() == null) return;
-        if (!GameruleHelper.getBool(Gamerules.REDSTONE_WIRE_ENABLED)) {
-            cir.setReturnValue(0);
-        }
-    }
-    /**
-     * @author DragonEggBedrockBreaking
-     * @reason modify the signal outputted
-     * @param blockState the state of the block
-     * @param blockGetter the block getter
-     * @param blockPos the position of the block
-     * @param direction the direction of the block
-     * @param cir the returnable callback info (Integer)
-     */
-    @Inject(method = "getDirectSignal", at = @At("HEAD"), cancellable = true)
-    private void modifyDirectSignal(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, Direction direction, CallbackInfoReturnable<Integer> cir) {
+    @Inject(method = "getWireSignal", at = @At("HEAD"), cancellable = true)
+    private void modifySignal(BlockState blockState, CallbackInfoReturnable<Integer> cir) {
         if (VDServer.getServer() == null) return;
         if (!GameruleHelper.getBool(Gamerules.REDSTONE_WIRE_ENABLED)) {
             cir.setReturnValue(0);
