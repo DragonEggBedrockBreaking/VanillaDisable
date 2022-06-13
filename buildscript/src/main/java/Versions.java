20d4a092f1b67c478c2ccf70bf222e50782f66e7
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-06-13 15:44:58 +0100

    Bump quilt loader version to compile against (0.17.0-beta.2 -> 0.17.1-beta.1) and dependency (0.16.1 -> 0.17.0)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,6 +1,6 @@
 public class Versions {
     public static String MINECRAFT_VERSION = "1.19";
-    public static String QUILT_LOADER_VERSION = "0.17.0-beta.2";
+    public static String QUILT_LOADER_VERSION = "0.17.1-beta.1";
     public static String MOD_VERSION = "2.0.0";
     public static int JAVA_VERSION = 17;
     public static String QUILTFLOWER_VERSION = "1.8.1";
