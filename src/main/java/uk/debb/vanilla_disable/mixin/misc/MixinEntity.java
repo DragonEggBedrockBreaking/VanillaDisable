04c718da508c1bc500a0bcd5150f3c2a1b4af342
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-06 10:49:59 +0100

    Improve some instanceof usages based on IntelliJ suggestions

--- src/main/java/uk/debb/vanilla_disable/mixin/misc/MixinEntity.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/misc/MixinEntity.java
@@ -4,7 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.vehicle.Boat;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,21 +26,23 @@ public abstract class MixinEntity {
     @Shadow
     public abstract boolean hurt(DamageSource source, float amount);
 
+    @Shadow
+    public abstract EntityType<?> getType();
+
     /**
      * @param state the state of the block below the entity
      * @param ci    the callback info
      * @author DragonEggBedrockBreaking
      * @reason kills boats when they hit blocks
      */
-    @Inject(method = "onInsideBlock", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onInsideBlock", at = @At("HEAD"))
     private void killOnHorizontalCollision(BlockState state, CallbackInfo ci) {
         if (VDServer.getServer() == null) return;
-        if (GameruleHelper.getBool(Gamerules.OLD_BOATS) && (Object) this instanceof Boat) {
+        if (GameruleHelper.getBool(Gamerules.OLD_BOATS) && this.getType() == EntityType.BOAT) {
             for (Direction direction : Direction.Plane.HORIZONTAL) {
                 BlockState blockState = this.level.getBlockState(this.blockPosition.relative(direction));
                 if (blockState.getMaterial().isSolid()) {
                     if (!this.hurt(DamageSource.GENERIC, Float.MAX_VALUE)) return;
-                    ci.cancel();
                 }
             }
         }
