f8922d561ba7ab74e51c605423e0ce34f4b9753c
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-09 22:20:10 +0100

    Remove useless code

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,7 +1,6 @@
 public class Versions {
     public static final String MINECRAFT_VERSION = "1.19";
     public static final String QUILT_LOADER_VERSION = "0.17.1-beta.2";
-    public static final String MOD_VERSION = "2.0.1";
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 
