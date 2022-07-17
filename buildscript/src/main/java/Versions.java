8748f436ea12b74e204da00600c9eec5283a454d
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-17 17:08:41 +0100

    Bump Quilt Loader (beta.2 -> beta.6) and QFAPI (beta.3 -> beta.6)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,10 +1,10 @@
 public class Versions {
     public static final String MINECRAFT_VERSION = "1.19";
-    public static final String QUILT_LOADER_VERSION = "0.17.1-beta.2";
+    public static final String QUILT_LOADER_VERSION = "0.17.1-beta.6";
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 
-    public static final String QFAPI_VERSION = "2.0.0-beta.3+0.57.0-1.19";
+    public static final String QFAPI_VERSION = "2.0.0-beta.6+0.57.0-1.19";
     public static final boolean JIJ_QFAPI = false;
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
