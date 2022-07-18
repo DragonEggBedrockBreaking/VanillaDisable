a523bda61a930817b2ab40cb989438b0372a09e6
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-18 19:09:45 +0100

    Use conditional mixin to prioritise tweakeroo's infinite trading code over mine

--- src/main/java/uk/debb/vanilla_disable/mixin/mob/MixinAbstractVillager_TradingEnabled.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/mob/MixinAbstractVillager_TradingEnabled.java
@@ -1,10 +1,7 @@
 package uk.debb.vanilla_disable.mixin.mob;
 
-import com.llamalad7.mixinextras.injector.ModifyReceiver;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.npc.AbstractVillager;
-import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.item.trading.MerchantOffers;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,27 +10,7 @@ import uk.debb.vanilla_disable.util.Gamerules;
 import uk.debb.vanilla_disable.util.VDServer;
 
 @Mixin(AbstractVillager.class)
-public abstract class MixinAbstractVillager {
-    /**
-     * @param receiver the original receiver
-     * @author DragonEggBedrockBreaking
-     */
-    @ModifyReceiver(
-            method = "notifyTrade",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/item/trading/MerchantOffer;increaseUses()V"
-            ),
-            require = 0
-    )
-    private MerchantOffer modifyUses(MerchantOffer receiver) {
-        if (VDServer.getServer() == null) return receiver;
-        if (GameruleHelper.getBool(Gamerules.INFINITE_TRADING)) {
-            return new MerchantOffer(new CompoundTag());
-        }
-        return receiver;
-    }
-
+public class MixinAbstractVillager_TradingEnabled {
     /**
      * @param original the original value
      * @author DragonEggBedrockBreaking
