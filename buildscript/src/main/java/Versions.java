b31a3ec5951810d51a349af7241bc26ac87d05d4
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-21 19:37:39 +0100

    Bump Minecraft (pre5 -> rc2)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,5 +1,5 @@
 public class Versions {
-    public static final String MINECRAFT_VERSION = "1.19.1-pre5";
+    public static final String MINECRAFT_VERSION = "1.19.1-rc2";
     public static final String QUILT_LOADER_VERSION = "0.17.1-beta.6";
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
