04b0c0c24e6f142d9cdb8dab70287865a0ae20fd
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-25 10:51:36 +0100

    Bump MixinExtras (0.0.11 -> 0.0.12)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -7,6 +7,6 @@ public class Versions {
     public static final String QFAPI_VERSION = "2.0.0-beta.9+0.58.0-1.19";
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
-    public static final String MIXIN_EXTRAS_VERSION = "0.0.11";
+    public static final String MIXIN_EXTRAS_VERSION = "0.0.12";
     public static final String CONDITIONAL_MIXIN_VERSION = "v0.3.0";
 }
