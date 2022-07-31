c6e587de1601cd877a1a7cb536fc81535f8ca308
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-31 12:04:56 +0100

    Bump Quilt Loader (0.17.1 -> 0.17.2-beta.2)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,6 +1,6 @@
 public class Versions {
     public static final String MINECRAFT_VERSION = "1.19";
-    public static final String QUILT_LOADER_VERSION = "0.17.1";
+    public static final String QUILT_LOADER_VERSION = "0.17.2-beta.2";
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 
