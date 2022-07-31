d8f55fa44b73a3fab623edfe482e6e295481ac69
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-07-31 15:55:47 +0100

    Fix 1.19.1 crash on world load

--- src/main/java/uk/debb/vanilla_disable/mixin/commands/MixinCommands.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/commands/MixinCommands.java
@@ -1,10 +1,12 @@
 package uk.debb.vanilla_disable.mixin.commands;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.brigadier.ParseResults;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import uk.debb.vanilla_disable.util.gamerules.BooleanGamerules;
@@ -14,14 +16,15 @@ import uk.debb.vanilla_disable.util.maps.Maps;
 @Mixin(Commands.class)
 public abstract class MixinCommands implements Maps {
     @ModifyReturnValue(method = "performCommand", at = @At(value = "RETURN"))
-    private int performCommand(int original, CommandSourceStack source, String command) {
+    private int performCommand(int original, ParseResults<CommandSourceStack> parseResults, String command) {
         String commandName = command.split(" ")[0];
         BooleanGamerules commandGamerule = commandsStringMap.get(commandName);
         BooleanGamerules dedicatedCommandGamerule = commandsStringMapDedicated.get(commandName);
+        MinecraftServer server = parseResults.getContext().getSource().getServer();
         if ((!command.startsWith("/gamerule") && !GameruleHelper.getBool(BooleanGamerules.COMMANDS_ENABLED)) ||
                 (commandGamerule != null && !GameruleHelper.getBool(commandGamerule)) ||
-                (source.getServer().isDedicatedServer() && dedicatedCommandGamerule != null && !GameruleHelper.getBool(dedicatedCommandGamerule))) {
-            source.getServer().getPlayerList().broadcastSystemMessage(Component.translatable("commands.disabled.by.vd").withStyle(ChatFormatting.RED), false);
+                (server.isDedicatedServer() && dedicatedCommandGamerule != null && !GameruleHelper.getBool(dedicatedCommandGamerule))) {
+            server.getPlayerList().broadcastSystemMessage(Component.translatable("commands.disabled.by.vd").withStyle(ChatFormatting.RED), false);
             return 0;
         }
         return original;
