e58619b77244b9c4a046b245f6f170ee373b216e
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-08-03 19:59:06 +0100

    Change `@Shadow` field in `MixinPainting` to satisfy IntelliJ

--- src/main/java/uk/debb/vanilla_disable/mixin/entity/MixinPainting.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/entity/MixinPainting.java
@@ -2,8 +2,11 @@ package uk.debb.vanilla_disable.mixin.entity;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.entity.decoration.Painting;
 import net.minecraft.world.entity.decoration.PaintingVariant;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,15 +16,14 @@ import uk.debb.vanilla_disable.util.maps.Maps;
 @Mixin(Painting.class)
 public abstract class MixinPainting implements Maps {
     @Shadow
-    private static Holder<PaintingVariant> getDefaultVariant() {
-        return null;
-    }
+    @Final
+    private static ResourceKey<PaintingVariant> DEFAULT_VARIANT;
 
     @ModifyReturnValue(method = "getVariant", at = @At("RETURN"))
     private Holder<PaintingVariant> modifyVariant(Holder<PaintingVariant> original) {
         Gamerules gameRule = paintingHolderPaintingVariantMap.get(original);
         if (!Gamerules.ALTERNATE_PAINTINGS_ENABLED.getBool() || (gameRule != null && !gameRule.getBool())) {
-            return getDefaultVariant();
+            return Registry.PAINTING_VARIANT.getHolderOrThrow(DEFAULT_VARIANT);
         }
         return original;
     }
