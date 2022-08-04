095ed608ac9d9671305c12b31703556c9caeff57
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-08-04 18:44:29 +0100

    Improve compatibility

--- src/main/java/uk/debb/vanilla_disable/mixin/blocks/MixinAbstractCauldronBlock.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/blocks/MixinAbstractCauldronBlock.java
@@ -1,42 +1,27 @@
 package uk.debb.vanilla_disable.mixin.blocks;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.AbstractCauldronBlock;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.debb.vanilla_disable.util.gamerules.Gamerules;
 import uk.debb.vanilla_disable.util.maps.Maps;
 
 @Mixin(AbstractCauldronBlock.class)
 public abstract class MixinAbstractCauldronBlock implements Maps {
-    /*@ModifyReceiver(
+    @ModifyExpressionValue(
             method = "use",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/core/cauldron/CauldronInteraction;interact(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/InteractionResult;"
+                    target = "Lnet/minecraft/world/entity/player/Player;getItemInHand(Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/item/ItemStack;"
             )
     )
-    private CauldronInteraction modifyUse(CauldronInteraction original, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, ItemStack itemStack) {
-        BooleanGamerules gameRule = abstractCauldronBlockItemMap.get(itemStack.getItem());
+    private ItemStack modifyItemStack(ItemStack original) {
+        Gamerules gameRule = abstractCauldronBlockItemMap.get(original.getItem());
         if (gameRule != null && !gameRule.getBool()) {
-            return (arg, arg2, arg3, arg4, arg5, arg6) -> InteractionResult.FAIL;
+            return ItemStack.EMPTY;
         }
         return original;
-    }*/
-
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void cancelUse(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        Gamerules gameRule = abstractCauldronBlockItemMap.get(player.getItemInHand(interactionHand).getItem());
-        if (gameRule != null && !gameRule.getBool()) {
-            cir.setReturnValue(InteractionResult.FAIL);
-        }
     }
 }
--- src/main/java/uk/debb/vanilla_disable/mixin/items/MixinItemStack.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/items/MixinItemStack.java
@@ -1,26 +1,28 @@
 package uk.debb.vanilla_disable.mixin.items;
 
-import net.minecraft.world.InteractionResultHolder;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.debb.vanilla_disable.util.gamerules.Gamerules;
 import uk.debb.vanilla_disable.util.maps.Maps;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack implements Maps {
-    @Shadow
-    public abstract Item getItem();
-
-    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
-    private void cancelUsage(CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        Gamerules gameRule = itemStackClassMap.get(this.getItem());
+    @ModifyExpressionValue(
+            method = "use",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"
+            )
+    )
+    private Item cancelUsage(Item original) {
+        Gamerules gameRule = itemStackClassMap.get(original);
         if (gameRule != null && !gameRule.getBool()) {
-            cir.setReturnValue(InteractionResultHolder.fail(new ItemStack(this.getItem())));
+            return Items.AIR;
         }
+        return original;
     }
 }
