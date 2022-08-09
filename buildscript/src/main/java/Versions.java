fe38064cf2caf643f7399286c0960897ce6d1493
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-08-09 11:12:33 +0100

    Bump Minecraft (1.19.1 -> 1.19.2) and QFAPI (beta.2+0.58.5-1.19.1 -> beta.7+0.59.0-1.19.2)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -1,10 +1,10 @@
 public class Versions {
-    public static final String MINECRAFT_VERSION = "1.19.1";
+    public static final String MINECRAFT_VERSION = "1.19.2";
     public static final String QUILT_LOADER_VERSION = "0.17.2-beta.2";
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 
-    public static final String QFAPI_VERSION = "4.0.0-beta.2+0.58.5-1.19.1";
+    public static final String QFAPI_VERSION = "4.0.0-beta.7+0.59.0-1.19.2";
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
     public static final String MIXIN_EXTRAS_VERSION = "0.0.12";
