28fd13481b4575ab5fe67a1b06a4351da2ad0648
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-09-07 20:49:15 +0100

    Bump QFAPI (beta.7+0.59.0 -> beta.11+0.60.0)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -4,7 +4,7 @@ public class Versions {
     public static final int JAVA_VERSION = 17;
     public static final String QUILTFLOWER_VERSION = "1.8.1";
 
-    public static final String QFAPI_VERSION = "4.0.0-beta.7+0.59.0-1.19.2";
+    public static final String QFAPI_VERSION = "4.0.0-beta.11+0.60.0-1.19.2";
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
     public static final String MIXIN_EXTRAS_VERSION = "0.1.0-rc5";
