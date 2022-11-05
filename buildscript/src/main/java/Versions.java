07a01497bd8d383e35b2e6ab34e320216e9818f5
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-11-05 21:55:04 +0000

    Bump Quiltflower (1.8.1 -> 1.9.0) and Mixin Extras (0.1.0-rc5 -> 0.1.1-rc.1)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -2,10 +2,10 @@ public class Versions {
     public static final String MINECRAFT_VERSION = "1.19.2";
     public static final String FABRIC_LOADER_VERSION = "0.14.10";
     public static final int JAVA_VERSION = 17;
-    public static final String QUILTFLOWER_VERSION = "1.8.1";
+    public static final String QUILTFLOWER_VERSION = "1.9.0";
     public static final String FABRIC_GAMERULE_API_VERSION = "1.0.22+c6af733c90";
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
-    public static final String MIXIN_EXTRAS_VERSION = "0.1.0-rc5";
+    public static final String MIXIN_EXTRAS_VERSION = "0.1.1-rc.1";
     public static final String CONDITIONAL_MIXIN_VERSION = "v0.3.1";
 }
