b0769c3b4a938ef63e72fceebb59d9444d35277f
DragonEggBedrockBreaking <antonbhaumik@proton.me>
2022-11-19 16:41:50 +0000

    Bump MixinExtras (0.1.1-rc.4 -> 0.1.1)

--- buildscript/src/main/java/Versions.java
+++ buildscript/src/main/java/Versions.java
@@ -6,6 +6,6 @@ public class Versions {
     public static final String FABRIC_GAMERULE_API_VERSION = "1.0.22+c6af733c90";
     public static final String CAFFEINE_CONFIG_VERSION = "1.0.0";
     public static final String LAZYDFU_VERSION = "0.1.3";
-    public static final String MIXIN_EXTRAS_VERSION = "0.1.1-rc.4";
+    public static final String MIXIN_EXTRAS_VERSION = "0.1.1";
     public static final String CONDITIONAL_MIXIN_VERSION = "v0.3.1";
 }
