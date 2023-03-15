962b5ba064c9a7abfed41ad72fddb3d5d350fa48
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-03-15 21:27:54 +0000

    Rewrite spawner gamerules to hopefully fix crash

--- src/main/java/uk/debb/vanilla_disable/mixin/spawning/MixinBaseSpawner.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/spawning/MixinBaseSpawner.java
@@ -1,35 +1,45 @@
 package uk.debb.vanilla_disable.mixin.spawning;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.BaseSpawner;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.SpawnData;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import uk.debb.vanilla_disable.util.gamerules.Gamerules;
 import uk.debb.vanilla_disable.util.maps.Maps;
 
+import java.util.function.Function;
+
 @Mixin(BaseSpawner.class)
 public abstract class MixinBaseSpawner implements Maps {
-    @Shadow
-    private @Nullable Entity displayEntity;
+    @Shadow protected abstract SpawnData getOrCreateNextSpawnData(@Nullable Level arg, RandomSource arg2, BlockPos arg3);
 
-    @Inject(
+    @ModifyExpressionValue(
             method = "serverTick",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/level/ServerLevel;tryAddFreshEntityWithPassengers(Lnet/minecraft/world/entity/Entity;)Z"
-            ),
-            cancellable = true
+                    target = "Lnet/minecraft/world/level/BaseSpawner;isNearPlayer(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)Z"
+            )
     )
-    private void cancelSpawningNewEntityAndPassengers(CallbackInfo ci) {
-        if (this.displayEntity != null) {
-            Gamerules gameRule = baseSpawnerClassMap.get(this.displayEntity.getClass());
+    private boolean cancelSpawningNewEntityAndPassengers(boolean original, ServerLevel level, BlockPos blockPos) {
+        CompoundTag compoundTag = this.getOrCreateNextSpawnData(level, level.getRandom(), blockPos).getEntityToSpawn();
+        Entity entity = EntityType.loadEntityRecursive(compoundTag, level, Function.identity());
+        System.out.println(entity);
+        if (entity != null) {
+            Gamerules gameRule = baseSpawnerClassMap.get(entity.getClass());
             if (!gameRule.getBool() || !Gamerules.SPAWNERS_ENABLED.getBool()) {
-                ci.cancel();
+                return false;
             }
         }
+        return original;
     }
 }
