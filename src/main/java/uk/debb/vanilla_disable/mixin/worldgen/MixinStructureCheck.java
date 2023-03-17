e63bcbf3248c19351d633d75c70a3b0dba7608b0
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-03-17 19:22:57 +0000

    Fix five broken mixins
    In four mixins, ModifyReturnValue doesn't work properly, so we need Inject + CallbackInfoReturnable instead
    
    In one mixin, ModifyExpressionValue doesn't work properly, so we need Redirect

--- src/main/java/uk/debb/vanilla_disable/mixin/worldgen/MixinStructureCheck.java
+++ src/main/java/uk/debb/vanilla_disable/mixin/worldgen/MixinStructureCheck.java
@@ -1,6 +1,5 @@
 package uk.debb.vanilla_disable.mixin.worldgen;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureCheck;
@@ -8,13 +7,15 @@ import net.minecraft.world.level.levelgen.structure.StructureCheckResult;
 import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import uk.debb.vanilla_disable.util.gamerules.Gamerules;
 import uk.debb.vanilla_disable.util.maps.Maps;
 
 @Mixin(StructureCheck.class)
 public abstract class MixinStructureCheck implements Maps {
-    @ModifyReturnValue(method = "checkStart", at = @At("RETURN"))
-    private StructureCheckResult cancelCheckingStart(StructureCheckResult original, ChunkPos chunkPos, Structure structure, boolean bl) {
+    @Inject(method = "checkStart", at = @At("HEAD"), cancellable = true)
+    private void cancelCheckingStart(ChunkPos chunkPos, Structure structure, boolean bl, CallbackInfoReturnable<StructureCheckResult> cir) {
         Gamerules gameRule;
         if (structure instanceof JigsawStructure jigsawStructure) {
             gameRule = structureCheckStringMap.get(jigsawStructure.startPool.unwrap().orThrow().location().toString());
@@ -22,8 +23,7 @@ public abstract class MixinStructureCheck implements Maps {
             gameRule = structureCheckStructureTypeMap.get(structure.type());
         }
         if (gameRule != null && !gameRule.getBool()) {
-            return StructureCheckResult.START_NOT_PRESENT;
+            cir.setReturnValue(StructureCheckResult.START_NOT_PRESENT);
         }
-        return original;
     }
 }
