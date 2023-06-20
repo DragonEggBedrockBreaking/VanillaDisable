80295753c296435422a80a8f8682539eeb5157a3
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-06-20 13:24:41 +0100

    Address an IntelliJ warning

--- src/main/java/uk/debb/vanilla_disable/command/mixin/rule/entity/other/MixinEntity.java
+++ src/main/java/uk/debb/vanilla_disable/command/mixin/rule/entity/other/MixinEntity.java
@@ -4,7 +4,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,9 +21,6 @@ public abstract class MixinEntity {
 
     @Shadow
     public abstract boolean hurt(DamageSource source, float amount);
-
-    @Shadow
-    public abstract EntityType<?> getType();
 
     @Shadow
     public abstract Level level();
