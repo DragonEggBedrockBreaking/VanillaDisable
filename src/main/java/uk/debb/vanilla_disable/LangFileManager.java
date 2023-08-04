f4407192bff30ff921cf155f579a8aa639fcad51
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2023-08-04 18:43:03 +0100

    The first time the language resource pack is created, enable it (if it is manually disabled later, leave it)

--- src/main/java/uk/debb/vanilla_disable/LangFileManager.java
+++ src/main/java/uk/debb/vanilla_disable/LangFileManager.java
@@ -1,5 +1,7 @@
 package uk.debb.vanilla_disable;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
@@ -11,9 +13,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -23,11 +27,31 @@ public class LangFileManager implements ClientModInitializer {
     public void onInitializeClient() {
         try {
             File outerrpackdir = new File(FabricLoader.getInstance().getGameDir().toString() + "/resourcepacks/vdlangfile");
+            boolean exists = outerrpackdir.exists();
             FileUtils.deleteDirectory(outerrpackdir);
 
             if (FabricLoader.getInstance().isModLoaded("fabric-resource-loader-v0") && !FabricLoader.getInstance().isModLoaded("quilt_resource_loader")) {
                 File rpackdir = new File(FabricLoader.getInstance().getGameDir().toString() + "/resourcepacks/vdlangfile/assets/vanilladisablelangfile/lang");
                 if (!rpackdir.mkdirs()) return;
+
+                int version = SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES);
+                String content = "{\"pack\":{\"pack_format\":" + version + ",\"description\":\"Vanilla Disable Language File\"}}";
+                String mcmetaPath = new File(outerrpackdir + "/pack.mcmeta").toString();
+                FileWriter mcmetaWriter = new FileWriter(mcmetaPath);
+                mcmetaWriter.write(content);
+                mcmetaWriter.close();
+
+                File optionsTxt = new File(FabricLoader.getInstance().getGameDir().toString() + "/options.txt");
+                String optionsTxtContent = FileUtils.readFileToString(optionsTxt, StandardCharsets.UTF_8);
+                ObjectList<String> lines = new ObjectArrayList<>();
+                Arrays.stream(optionsTxtContent.split("\n")).forEach(line -> {
+                    if (line.startsWith("resourcePacks") && !line.contains("vdlangfile") && !exists) {
+                        lines.add(line.replace("]", ",\"file/vdlangfile\"]"));
+                    } else {
+                        lines.add(line);
+                    }
+                });
+                FileUtils.write(optionsTxt, String.join("\n", lines), StandardCharsets.UTF_8);
 
                 Thread thread = new Thread(() -> {
                     while (Minecraft.getInstance().getLanguageManager() == null) {}
@@ -51,13 +75,6 @@ public class LangFileManager implements ClientModInitializer {
                     });
                 });
                 thread.start();
-
-                int version = SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES);
-                String content = "{\"pack\":{\"pack_format\":" + version + ",\"description\":\"Vanilla Disable Language File\"}}";
-                String mcmetaPath = new File(outerrpackdir + "/pack.mcmeta").toString();
-                FileWriter mcmetaWriter = new FileWriter(mcmetaPath);
-                mcmetaWriter.write(content);
-                mcmetaWriter.close();
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
