92b0422a226f28e1e47f3ae2d5d16da2fb69706c
DragonEggBedrockBreaking <68545280+DragonEggBedrockBreaking@users.noreply.github.com>
2025-02-16 16:41:18 +0000

    Bump Neoforge (21.4.86-beta -> 21.4.89-beta)

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ val MIXIN_EXTRAS_VERSION by extra { "0.4.1" }
 val FABRIC_MIXIN_VERSION by extra { "0.15.5+mixin.0.8.7" }
 val FABRIC_LOADER_VERSION by extra { "0.16.10" }
 val FABRIC_LOADER_VERSION_RANGE by extra { ">=0.16.9" }
-val NEOFORGE_VERSION by extra { "21.4.87-beta"}
+val NEOFORGE_VERSION by extra { "21.4.89-beta"}
 val NEOFORGE_LOADER_VERSION_RANGE by extra { ">=21.4.0-beta" }
 
 allprojects {
